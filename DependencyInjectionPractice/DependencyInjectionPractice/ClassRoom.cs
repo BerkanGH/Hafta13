9fad381aff1ed171b3ee4218c83ef5e73d965780
BerkanGH <berkankeskin28@gmail.com>
2025-01-15 10:39:07 +0300

    odev

---
+++ DependencyInjectionPractice/DependencyInjectionPractice/ClassRoom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionPractice
+{
+    public class ClassRoom
+    {
+        private readonly ITeacher _teacher;
+
+        // Constructor Injection ile Ogretmen instance'ı alıyoruz
+        public ClassRoom(ITeacher teacher)
+        {
+            _teacher = teacher;
+        }
+
+        // Teacher nesnesinin GetInfo() metodu üzerinden ogretmen bilgilerini döndüren metot
+        public string GetTeacherInfo()
+        {
+            return _teacher.GetInfo();
+        }
+    }
+}
---
+++ DependencyInjectionPractice/DependencyInjectionPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using DependencyInjectionPractice;
+
+class Program
+{
+    static void Main()
+    {
+        // Öncelikle bir Ogretmen instance'ı oluşturuyoruz
+        ITeacher teacher = new Teacher("Burak", "Can");
+
+        // ClassRoom sınıfını oluştururken oluşturduğumuz Ogretmen instance'ını constructor'a geçiyoruz
+        ClassRoom classroom = new ClassRoom(teacher);
+
+        // ClassRoom sınıfının GetTeacherInfo() metodu üzerinden ogretmen bilgilerini alıp ekrana yazdırıyoruz
+        Console.WriteLine("Öğretmen Bilgileri:");
+        Console.WriteLine(classroom.GetTeacherInfo());
+    }
+}
